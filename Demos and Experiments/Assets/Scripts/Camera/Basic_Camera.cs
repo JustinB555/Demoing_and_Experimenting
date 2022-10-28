e67dc43a2515df12488518421520102e746aa265
JustinB555 <justin.b208@gmail.com>
2022-10-27 20:15:42 -0700

    Start on Camera stuff

---
+++ Demos and Experiments/Assets/Scripts/Camera/Basic_Camera.cs
@@ -0,0 +1,22 @@
+/* Created by: Justin Butler
+ * Purpose: To showcase the basic ways that you can use the camera.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basic_Camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
